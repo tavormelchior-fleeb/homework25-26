1aa2393385b12caa9fa7f420518d40867990d8f1
tavormelchior-fleeb <tavormelchior@gmail.com>
2025-10-12 19:26:29 +0300

    Update homework_21.9.py

--- homework_21.9/homework_21.9.py
+++ homework_21.9/homework_21.9.py
@@ -14,9 +14,13 @@ morse_to_english = {
     '...-.-': 'SOS'
         }
 
+# all letters in caps
+abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def read_morse_file(file_path):
-    with open(file_path, "r") as f:
+
+def read_morse_file(file):
+    letter_counter = {let: 0 for let in abc}
+    with open(file, "r") as f:
         # read the file and split it into words, removing "/n" from the end
         file_contents = f.read()[:-1].split(" / ")
     index = 0
@@ -25,19 +29,25 @@ def read_morse_file(file_path):
         for ch in word.split(" "):
             try:
                 words_lst[index] += morse_to_english[ch]
+                letter_counter[morse_to_english[ch]] += 1
             except KeyError:
                 print("error in morse code")
                 return
 
         index += 1
     print(" ".join(words_lst))
+    # make set with values of counts from dict, group letters that have said count, and print.
+    # cut letters with count 0, sort in descending order
+    for value in sorted(set(letter_counter.values()), reverse=True)[:-1]:
+        group = [k for k, v in letter_counter.items() if v == value]
+        group.sort()
+        print(f"{"".join(group)} - {value}")
 
 
 read_morse_file("morse1.txt")
 read_morse_file("morse2.txt")
 read_morse_file("morse-err1.txt")
 read_morse_file("morse-err2.txt")
-
 
 
 
