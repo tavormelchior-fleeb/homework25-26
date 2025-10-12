b4c5c5db4c0f3e490588a01094b4d9b45c608053
tavormelchior-fleeb <tavormelchior@gmail.com>
2025-10-12 15:15:43 +0300

    Add files via upload
    homework from september 21st, and example text files

---
+++ homework_21.9/homework_21.9.py
@@ -0,0 +1,39 @@
+# morse dict from internet
+morse_to_english = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    '.-.-.-': '.', '--..--': ',', '..--..': '?', '---...': ':',
+    '-.-.-.': ';', '-....-': '-', '.-..-.': '"', '.-.-.': '+',
+    '-..-.': '/', '.-.-..': '@', '-.--.': '(', '-.--.-': ')',
+    '...-.-': 'SOS'
+        }
+
+
+def read_morse_file(file_path):
+    with open(file_path, "r") as f:
+        file_contents = f.read().split("/")
+    index = 0
+    words_lst = ["" for i in range(len(file_contents))]
+    for word in file_contents:
+        for ch in word:
+            if ch not in morse_to_english.keys():
+                print("Error in Morse Code")
+                return
+            else:
+                words_lst[index] += ch
+        index += 1
+    print(" ".join(words_lst))
+
+
+
+read_morse_file("C:\תבור\בית ספר\כיתה יב\סייבר\משימות רנדומליות\homework_21.9\morse1.txt")
+
+
+
+
