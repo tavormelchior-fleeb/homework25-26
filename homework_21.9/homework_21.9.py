e170825bed1a5deda77b7b9160cfca1763990333
tavormelchior-fleeb <tavormelchior@gmail.com>
2025-10-12 18:13:52 +0300

    Update homework_21.9.py

--- homework_21.9/homework_21.9.py
+++ homework_21.9/homework_21.9.py
@@ -17,22 +17,26 @@ morse_to_english = {
 
 def read_morse_file(file_path):
     with open(file_path, "r") as f:
-        file_contents = f.read().split("/")
+        # read the file and split it into words, removing "/n" from the end
+        file_contents = f.read()[:-1].split(" / ")
     index = 0
     words_lst = ["" for i in range(len(file_contents))]
     for word in file_contents:
-        for ch in word:
-            if ch not in morse_to_english.keys():
-                print("Error in Morse Code")
+        for ch in word.split(" "):
+            try:
+                words_lst[index] += morse_to_english[ch]
+            except KeyError:
+                print("error in morse code")
                 return
-            else:
-                words_lst[index] += ch
+
         index += 1
     print(" ".join(words_lst))
 
 
-
-read_morse_file("")
+read_morse_file("morse1.txt")
+read_morse_file("morse2.txt")
+read_morse_file("morse-err1.txt")
+read_morse_file("morse-err2.txt")
 
 
 
